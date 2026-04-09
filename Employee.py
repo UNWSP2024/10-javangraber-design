77694c2018f513aee7ae981a9770f76d5851f027
javangraber-design <javangraber@gmail.com>
2026-04-09 15:13:51 -0500

    Update Employee.py

--- Employee.py
+++ Employee.py
@@ -1,10 +1,75 @@
+# Programmer: Javan Graber
+# Date: 4/9/2026
 # Program #4 Employee Class:
-# Write a class Employee that holds the following data about an employee in attributes: name, ID number, department, and job title.
 
-# Once you have written the class, write a program that creates three Employee objects to hold the following data.
+# Create the Employee class that will save the given employee information and
+# then give them
+class Employee:
+    # Begin with the initializer method
+    def __init__(self, name, id_number, department, job_title):
+        self.__name = name
+        self.__ID_number = id_number
+        self.__department = department
+        self.__job_title = job_title
+    # Create the functions for returning the employee information
+    def get_name(self):
+        return self.__name
+    def get_id_number(self):
+        return self.__ID_number
+    def get_department(self):
+        return self.__department
+    def get_job_title(self):
+        return self.__job_title
 
-# Name	ID Number	Department	Job Title
-# Susan Meyers	47899 	Accounting	Vice President
-# Mark Jones	39119	IT	Programmer
-# Joy Rogers	81774	Manufacturing	Engineer
-# The program should store the data in the three objects, then display the data for each employee on the screen.
+# Define the main function that gets the user input, calls for the Employee Class, and
+# Displays the information
+def main():
+        # Enter the first employee information
+        name1 = "Susan Meyers"
+        id_number1 = 47899
+        department1 = "Accounting"
+        job_title1 = "Vice President"
+        # Do the first call for the Employee class
+        employee1 = Employee(name1, id_number1, department1, job_title1)
+
+        # Print the information
+        print("Here is the information for the first employee:")
+        print(f"Name: {employee1.get_name()}")
+        print(f"ID_number: {employee1.get_id_number()}")
+        print(f"Department: {employee1.get_department()}")
+        print(f"Job title: {employee1.get_job_title()}")
+        print("")
+
+        # Enter the second employee information
+        name2 = "Mark Jones"
+        id_number2 = 39119
+        department2 = "IT"
+        job_title2 = "Programmer"
+        # Do the second call for the Employee class
+        employee2 = Employee(name2, id_number2, department2, job_title2)
+
+        print("Here is the information for the second employee:")
+        print(f"Name: {employee2.get_name()}")
+        print(f"ID_number: {employee2.get_id_number()}")
+        print(f"Department: {employee2.get_department()}")
+        print(f"Job title: {employee2.get_job_title()}")
+        print("")
+
+        # Enter the third employee information
+        name3 = "Joy Rogers"
+        id_number3 = 81774
+        department3 = "Manufacturing"
+        job_title3 = "Engineer"
+        # Do the third call for the Employee Class
+        employee3 = Employee(name3, id_number3, department3, job_title3)
+
+        print("Here is the information for the third employee:")
+        print(f"Name: {employee3.get_name()}")
+        print(f"ID_number: {employee3.get_id_number()}")
+        print(f"Department: {employee3.get_department()}")
+        print(f"Job title: {employee3.get_job_title()}")
+        print("")
+
+
+if __name__ == "__main__":
+    main()
